867248f4aea50f63e098744159c94234faf87e17
ThomasGalliot <t.galliot1@gmail.com>
2019-10-15 15:59:04 +0200

    modification du fichier sql

--- bdd.sql
+++ bdd.sql
@@ -7,6 +7,20 @@
 CREATE DATABASE IF NOT EXISTS `evolutioerevog` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `evolutioerevog`;
 
+CREATE TABLE IF NOT EXISTS `constructeur` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  `date_de_creation` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+DELETE FROM `constructeur`;
+/*!40000 ALTER TABLE `constructeur` DISABLE KEYS */;
+INSERT INTO `constructeur` (`id`, `nom`, `date_de_creation`) VALUES
+	(1, 'Nintendo', '1889-09-23'),
+	(2, 'SEGA', '1960-06-03');
+/*!40000 ALTER TABLE `constructeur` ENABLE KEYS */;
+
 CREATE TABLE IF NOT EXISTS `console` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(100) NOT NULL,
@@ -20,25 +34,11 @@ CREATE TABLE IF NOT EXISTS `console` (
 DELETE FROM `console`;
 /*!40000 ALTER TABLE `console` DISABLE KEYS */;
 INSERT INTO `console` (`id`, `nom`, `date_de_creation`, `constructeur_id`) VALUES
-	(1, 'NES', NULL, 1),
-	(2, 'megadrive', NULL, 2),
-	(3, 'SNES', NULL, 1),
-	(4, 'Nintento64', NULL, 1);
+	(1, 'NES', '1983-07-15', 1),
+	(2, 'megadrive', '1988-10-29', 2),
+	(3, 'SNES', '1990-11-21', 1),
+	(4, 'Nintento64', '1996-06-23', 1);
 /*!40000 ALTER TABLE `console` ENABLE KEYS */;
-
-CREATE TABLE IF NOT EXISTS `constructeur` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(100) NOT NULL,
-  `date_de_creation` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
-DELETE FROM `constructeur`;
-/*!40000 ALTER TABLE `constructeur` DISABLE KEYS */;
-INSERT INTO `constructeur` (`id`, `nom`, `date_de_creation`) VALUES
-	(1, 'Nintendo', NULL),
-	(2, 'SEGA', NULL);
-/*!40000 ALTER TABLE `constructeur` ENABLE KEYS */;
 
 CREATE TABLE IF NOT EXISTS `jeux_videos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -52,10 +52,12 @@ CREATE TABLE IF NOT EXISTS `jeux_videos` (
   PRIMARY KEY (`id`),
   KEY `console_id` (`console_id`),
   CONSTRAINT `jeux_videos_ibfk_1` FOREIGN KEY (`console_id`) REFERENCES `console` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 DELETE FROM `jeux_videos`;
 /*!40000 ALTER TABLE `jeux_videos` DISABLE KEYS */;
+INSERT INTO `jeux_videos` (`id`, `image`, `nom`, `region`, `developpeur`, `editeur`, `date_de_creation`, `console_id`) VALUES
+	(1, 'img/jeuxVideos/images.jpg', 'The Legend of Zelda: Ocarina of Time', 'PAL-FAH', 'Nintendo', 'Nintendo', '1998-11-21', 4);
 /*!40000 ALTER TABLE `jeux_videos` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
