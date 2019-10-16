0bf4ccd967347b5712f6143e3877dcb834334089
ThomasGalliot <t.galliot1@gmail.com>
2019-10-16 08:51:59 +0200

    modif bdd.sql

--- bdd.sql
+++ bdd.sql
@@ -7,6 +7,20 @@
 CREATE DATABASE IF NOT EXISTS `evolutioerevog` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `evolutioerevog`;
 
+CREATE TABLE IF NOT EXISTS `constructeur` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  `date_de_creation` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+DELETE FROM `constructeur`;
+/*!40000 ALTER TABLE `constructeur` DISABLE KEYS */;
+INSERT INTO `constructeur` (`id`, `nom`, `date_de_creation`) VALUES
+	(1, 'Nintendo', '1889-09-23'),
+	(2, 'SEGA', '1960-06-03');
+/*!40000 ALTER TABLE `constructeur` ENABLE KEYS */;
+
 CREATE TABLE IF NOT EXISTS `console` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(100) NOT NULL,
@@ -26,20 +40,6 @@ INSERT INTO `console` (`id`, `nom`, `date_de_creation`, `constructeur_id`) VALUE
 	(4, 'Nintento64', '1996-06-23', 1);
 /*!40000 ALTER TABLE `console` ENABLE KEYS */;
 
-CREATE TABLE IF NOT EXISTS `constructeur` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(100) NOT NULL,
-  `date_de_creation` date NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
-DELETE FROM `constructeur`;
-/*!40000 ALTER TABLE `constructeur` DISABLE KEYS */;
-INSERT INTO `constructeur` (`id`, `nom`, `date_de_creation`) VALUES
-	(1, 'Nintendo', '1889-09-23'),
-	(2, 'SEGA', '1960-06-03');
-/*!40000 ALTER TABLE `constructeur` ENABLE KEYS */;
-
 CREATE TABLE IF NOT EXISTS `jeux_videos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `image` varchar(255) NOT NULL,
@@ -51,12 +51,20 @@ CREATE TABLE IF NOT EXISTS `jeux_videos` (
   PRIMARY KEY (`id`),
   KEY `console_id` (`console_id`),
   CONSTRAINT `jeux_videos_ibfk_1` FOREIGN KEY (`console_id`) REFERENCES `console` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 DELETE FROM `jeux_videos`;
 /*!40000 ALTER TABLE `jeux_videos` DISABLE KEYS */;
 INSERT INTO `jeux_videos` (`id`, `image`, `nom`, `developpeur`, `editeur`, `date_de_creation`, `console_id`) VALUES
-	(1, 'img/jeuxVideos/images.jpg', 'The Legend of Zelda: Ocarina of Time', 'Nintendo', 'Nintendo', '1998-11-21', 4);
+	(1, 'img/jeuxVideos/zeldaOOT.jpg', 'The Legend of Zelda: Ocarina of Time', 'Nintendo EAD', 'Nintendo', '1998-11-21', 4),
+	(2, 'img/jeuxVideos/zeldaMM.jpg', 'The Legend of Zelda: Majora\'s Mask', 'Nintendo EAD', 'Nintendo', '2000-04-27', 4),
+	(3, 'img/jeuxVideos/007LeMondeNeSuffitPas.jpg', '007 Le Monde Ne Suffit Pas', 'Eurocom', 'EA Games, MGM Interactive', '2000-10-17', 4),
+	(4, 'img/jeuxVideos/1080Snowboarding.jpg', '1080° Snowboarding', 'Nintendo EAD', 'Nintendo', '1998-02-28', 4),
+	(5, 'img/jeuxVideos/64Ozumo.jpg', '64 Ōzumō', 'Bottom Up', 'Bottom Up', '1997-11-28', 4),
+	(6, 'img/jeuxVideos/64Ozumo2.jpg', '64 Ōzumō 2', 'Bottom Up', 'Bottom Up', '1999-03-19', 4),
+	(7, 'img/jeuxVideos/64deHakken!TamagotchiMinnadeTamagotchi.jpg', '64 de Hakken! Tamagotchi Minna de Tamagotchi...', 'AI', 'Bandai', '1997-12-19', 4),
+	(8, 'img/jeuxVideos/64HanafudaTenshinoYakusoku.jpg', '64 Hanafuda: Tenshi no Yakusoku', 'Altron', 'Altron', '1999-11-05', 4),
+	(9, 'img/jeuxVideos/64TrumpCollectionAlicenoWakuWakuTrump.jpg', '64 Trump Collection: Alice no Waku Waku Trump...', 'Bottom Up', 'Bottom Up', '1998-08-07', 4);
 /*!40000 ALTER TABLE `jeux_videos` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
