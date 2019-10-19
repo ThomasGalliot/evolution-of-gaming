ee5f9e9d03b64b6ce0cb2835f018422755e62125
dustyrix <godineau.pro@gmail.com>
2019-10-19 20:13:05 +0200

    Add hero

--- index.php
+++ index.php
@@ -12,16 +12,12 @@
         <title>Evolution Of Gaming</title>
     </head>
     <body class="background-body">
+
         <?php include("navbar.php");?>
+
+        <?php include("hero.php"); ?>
+
         <div class="col-10 m-auto shadow-form">
-            <div>
-                <h1 style="text-align: center;">Evolution Of Gaming</h1>
-                <p>Bonjour et bienvenue sur Evolution Of Gaming, Nous tenons à signaler qu'il s'agit d'une version alpha, sur ce site
-                nous allons référencer tout les jeux pour chaque console pour toutes les régions, ainsi que tout les bundles et
-                toute les éditions limitées pour chaque jeux et consoles de jeu. Il y aura aussi plein de petites choses sur chaque
-                jeux, comme des easter eggs, des glitchs, etc..., tout ça, c'est pour la première version. si vous voyez des oublis
-                sur quelque chose n'hésitez pas à nous le faire savoir sur notre discord que voici : <a href="https://discord.gg/2DEcgqU">discord</a></p>
-            </div>
             <div class="offset-5">
                 <div class="input-group mb-3">
                     <form action="#" method="post">
@@ -67,7 +63,7 @@
                 include("php/bdd/sortie/affichageJeux.php");
             ?>
         </div>
-        <?php include("footer.php") ?>
+        <?php include("footer.php"); include("php/view/cdn/js/bootstrap.php");?>
         <script type="text/javascript" src="js/modifSelectConsole.js"></script>
         <script type="text/javascript" src="js/modifSelectRegion.js"></script>
     </body>
